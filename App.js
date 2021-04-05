aff9400e88bfccde5cef5e460ff086142b51608a
studyChan <midifei1d2r_@naver.com>
2021-04-05 10:04:42 +0900

    04/03실습 및 README.md 작성

--- App.js
+++ App.js
@@ -5,43 +5,54 @@
  * @format
  * @flow strict-local
  */
-
+// 예 2.16
 import React from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   ScrollView,
-  View,
-  Text,
   StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
   DebugInstructions,
+  Header,
+  LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-class App extends React.Component {
-  render() {
-    let book = "React Native in Action"
-    return (
-        <BookDisplay book={book}/>
-    )
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={loading:true, data:{}}
   }
-}
-
-class BookDisplay extends React.Component {
-  render() {
-    return(
-    <View>
-      <Text>{this.props.book}</Text>
-    </View>
-    )
+  componentDidMount(){
+    //   #simulate ajex call
+      setTimeout(()=>{
+          this.setState({
+              loading:false,
+              data:{name:'Nader Dabit',age:35}
+          })
+      },2000)
   }
+    render(){
+        if(this.state.loading){
+            return <Text>Loading</Text>
+        }
+        const {name,age} = this.state.data
+        return(
+            <View >
+                <Text>name:{name}</Text>
+                <Text>age:{age}</Text>
+            </View>
+        )
+    }
 }
 
 export default App;
+
