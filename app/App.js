02244c838a25232727758b45d26ca8907cbff02c
studyChan <midifei1d2r_@naver.com>
2021-04-16 15:47:06 +0900

    04/16 실습 및 README.md 작성

--- app/App.js
+++ app/App.js
@@ -3,6 +3,8 @@
  import Heading from './Heading';
  import Input from './Input';
  import Button from './Button';
+ import TodoList from './TodoList';
+ import TabBar from './TabBar'
 
  let todoIndex = 0
  
@@ -14,6 +16,14 @@
              todos: [],
              type: 'All'
          }
+         this.submitTodo = this.submitTodo.bind(this)
+         this.toggleComplete = this.toggleComplete.bind(this)
+         this.deleteTodo = this.deleteTodo.bind(this)
+         this.setType = this.setType.bind(this)
+     }
+
+     setType(type) {
+       this.setState({type})
      }
 
      inputChange(inputValue) {
@@ -38,18 +48,41 @@
          })
      }
 
+     deleteTodo (todoIndex) {
+       let {todos} = this.state
+       todos = todos.filter((todo) => todo.todoIndex !== todoIndex)
+       this.setState({todos})
+     }
+
+     toggleComplete (todoIndex) {
+      let todos = this.state.todos
+      todos.forEach((todo) => {
+        if (todo.todoIndex === todoIndex) {
+          todo.complete = !todo.complete
+        }
+      })
+      this.setState({todos})
+      }
+
      render() {
-         const { inputValue } = this.state;
+         const { inputValue, todos, type} = this.state;
        return (
          <View style={styles.container}>
            <ScrollView 
-           keyboardShouldPersistTaps='always'
-           style={styles.content}>
-           <Heading />
-           <Input
-           inputValue={inputValue}
-           inputChange={(text) => this.inputChange(text)} />
+            keyboardShouldPersistTaps='always'
+            style={styles.content}>
+            <Heading />
+            <Input
+            inputValue={inputValue}
+            inputChange={(text) => this.inputChange(text)} />
+            <TodoList 
+            type = {type}
+            toggleComplete={this.toggleComplete}
+            deleteTodo={this.deleteTodo}
+            todos={todos} />
+            <Button submitTodo={this.submitTodo}/>
            </ScrollView>
+           <TabBar type={type} setType={this.setType}/>
          </View>
        )
      }
